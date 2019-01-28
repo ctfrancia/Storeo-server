a4e8748ecb49fe49d24bcaa7b89037f3435883e1
cervante <luca.panzavolta@gmail.com>
2019-01-28 17:19:38 +0100

    Added all necessary routes for admins and users.

---
+++ Routes/admin_routes.js
@@ -0,0 +1,31 @@
+
+//================================
+//         ADMIN ROUTES
+//================================
+
+//PRODUCTS
+
+//get /admin/products
+//post /admin/products/
+
+//SPECIFIC PRODUCT
+//get /admin/products/:productId
+//put /admin/products/:productId
+//delete /admin/products/:productId
+
+//ORDERS
+//get /admin/orders
+
+//CATEGORIES
+//get /admin/category 
+
+//SPECIFIC CATEGORY
+//post /admin/category/:categoryId
+//put /admin/category/:categoryId
+//delete /admin/category/:categoryId
+
+//LOGIN
+//post /admin/login
+
+//SIGNUP
+//post /admin/signup
---
+++ Routes/user_routes.js
@@ -0,0 +1,31 @@
+
+//================================
+//         USER ROUTES
+//================================
+
+//PRODUCTS
+//get /products
+
+//SPECIFIC PRODUCT
+//get /products/:productId
+
+//CATEGORIES
+//get /category 
+
+//PRODUCTS BY CATEGORY
+//get /products/:categoryId
+
+//ORDERS
+//post /orders
+
+//SIGNUP
+//post /signup
+
+//LOGIN
+//post /login
+
+//SEARCH
+//post /search 
+
+//PREVIOUS ORDERS ???
+//get /orders
