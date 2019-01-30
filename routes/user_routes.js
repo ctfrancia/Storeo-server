f191edc541099f5ee449dc0f2f07b325b13842a7
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-01-30 08:10:49 +0100

    Edit the `user_routes` and remove unused getProductsByCategory controller

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,32 +2,21 @@ import express from 'express';
 import getAllProducts from '../Controllers/UserControllers/getAllProducts';
 import getProductsById from '../Controllers/UserControllers/getProductsById';
 import getAllCategories from '../Controllers/UserControllers/getAllCategories';
-import getProductsByCategory from '../Controllers/UserControllers/getProductsByCategory';
 import postNewOrder from '../Controllers/UserControllers/postNewOrder';
 import getProductsByCategoryId from '../Controllers/UserControllers/getProductByCategoryId';
 
 const router = express.Router();
 
-//  GET ALL PRODUCTS - DONE
+//  Products
 router.get('/products', getAllProducts);
-
-//  GET SPECIFIC PRODUCT BY ID - DONE
 router.get('/products/:productId', getProductsById);
-
-// GET SPECIFIC PRODUCT BY CATEGORY ID - DONE
 router.get('/products/cat/:categoryId', getProductsByCategoryId);
 
-// GET ALL CATEGORIES - DONE
+// Categories
 router.get('/categories', getAllCategories);
 
-// PRODUCTS BY CATEGORY
-// get /products/:categoryId
-
-router.get('/categories/:categoryId', getProductsByCategory);
-
-// ORDERS
+// Orders
 // post /orders
-
 router.post('/orders', postNewOrder);
 
 // SIGNUP
