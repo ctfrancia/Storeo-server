e5bd64fde87fd64959b08a3a0978d26de7f719fa
cervante <luca.panzavolta@gmail.com>
2019-01-30 22:36:15 +0100

    userLogin Controller completed.

---
+++ Controllers/UserControllers/userLogin.js
@@ -0,0 +1,58 @@
+import bcrypt from 'bcrypt';
+import atob from 'atob';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import log from '../../Helpers/log';
+import sequelize from '../../db';
+import User from '../../Models/UserModel';
+
+dotenv.config();
+
+const userLogin = async (req, res) => {
+  const [authType, encodedString] = req.headers.authorization.split(' ');
+  if (authType === 'Basic') {
+    const [email, password] = atob(encodedString).split(':');
+
+    const [user] = await sequelize.query('SELECT id, first_name, last_name, password FROM users_new WHERE email = :email',
+      { replacements: { email }, type: sequelize.QueryTypes.SELECT });
+
+    log('user is ', user);
+    //  The following block of code runs if the user with that email is found
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      log('isPasswordValid', isPasswordValid);
+      if (isPasswordValid) {
+        const jwtSecret = process.env.JWT_SECRET;
+        // Token generation
+        const token = jwt.sign({ email }, jwtSecret);
+        // Insert token for user in the DB
+        await sequelize.query(
+          'UPDATE users_new SET auth_token = ? WHERE email = ?',
+          {
+            model: User,
+            replacements: [token, email],
+            type: sequelize.QueryTypes.INSERT,
+          },
+        );
+        // Deleting password property on user object before sending to client
+        delete user.password;
+        user.token = token;
+
+        res
+          .status(200)
+          .send(user);
+      } else {
+        res
+          .status(400)
+          .send('Wrong password.');
+      }
+    } else {
+      res
+        .status(400)
+        .send('No user is associated to the given email.');
+    }
+  }
+};
+
+export default userLogin;
