b3ab50ac9af439d4b32be5ffd78786f8fe3f8ba3
Christian Francia <CTFrancia@gmail.com>
2019-01-30 16:29:53 +0100

    add admin model

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,6 +1,13 @@
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      unique: true,
+    },
     password: {
       allowNull: false,
       type: DataTypes.STRING,
