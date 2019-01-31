95f024cfef9c0cab34fc8f585f121e39a7e0e840
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-02-01 00:59:18 +0100

    Add createNewCategory controller

--- Controllers/AdminControllers/createNewCategory.js
+++ Controllers/AdminControllers/createNewCategory.js
@@ -1,38 +1,57 @@
 import Sequelize from 'sequelize';
 
 import Category from '../../Models/CategoryModel';
+import CategoryProperties from '../../Models/Category_PropertiesModel';
 import sequelize from '../../db';
 
 
 const createNewCategory = async (req, res) => {
-  const {
-    category_propertes: categoryProps,
-    name: categoryName,
-  } = req.body;
-  try {
-    const newCategory = await sequelize.query(
-      `INSERT INTO categories (name)
-       VALUES ( :categoryName)
-      `,
-      {
-        model: Category,
-        replacements: { categoryName },
-        type: Sequelize.QueryTypes.INSERT,
-      },
-    );
+  const { categoryProperties, name } = req.body;
 
-    //  GET ID from the created newCategory
-    const { property_name: propName, units } = categoryProps;
+  //  Create new Category
+  const newCategory = await sequelize.query(
+    `INSERT INTO categories (name)
+    VALUES (:name)
+    `,
+    {
+      model: Category,
+      replacements: { name },
+      type: Sequelize.QueryTypes.INSERT,
+    },
+  );
 
-    const insertProperties = await sequelize.query(
-      `INSERT INTO category_properties 
-      (property_name, units, category_id )
-      VALUES ( :propName, :units, :categoryId )
-      `,
-    );
-  } catch (error) {
+  // Get the id of the created category
+  const newCategoryId = newCategory[0];
+  let insertError = false;
 
-  }
+  // Insert all the category properties from the array - dynamicaly
+  // Promise.all returns a single promise that resolves when all requests initiated by `map` are finished
+  await Promise.all(categoryProperties.map(async (propertObj) => {
+    const { propertyName, units } = propertObj;
+    try {
+      await sequelize.query(
+        `INSERT INTO category_properties 
+        (property_name, units, category_id )
+        VALUES ( :propertyName, :units, :newCategoryId )
+        `,
+        {
+          model: CategoryProperties,
+          replacements: { propertyName, units, newCategoryId },
+          type: Sequelize.QueryTypes.INSERT,
+        },
+      );
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error in createNewCategory controller', error);
+      insertError = true;
+    }
+  }));
+
+  const requestStatus = insertError ? 500 : 201;
+  const responseMessage = 201 ? 'OK' : 'Request Error';
+  res
+    .status(requestStatus)
+    .send(responseMessage);
 };
 
 export default createNewCategory;
