026985bba58e2a18983b09d73e72ac75b60846b1
cervante <luca.panzavolta@gmail.com>
2019-01-31 13:42:14 +0100

    Refactor involving raw sql query complete on getAllCategories controller.

--- Controllers/UserControllers/getAllCategories.js
+++ Controllers/UserControllers/getAllCategories.js
@@ -4,8 +4,8 @@ const getAllCategories = async (req, res) => {
   try {
     const categories = await sequelize.query(
       `SELECT categories.id, categories.name, 
-       GROUP_CONCAT(category_properties.property_name) AS properties, 
-       GROUP_CONCAT(category_properties.units) AS property_values
+       GROUP_CONCAT(category_properties.property_name) AS property_names, 
+       GROUP_CONCAT(category_properties.units) AS property_units
        FROM categories, category_properties
        WHERE categories.id = category_properties.category_id GROUP BY categories.id;`,
     );
