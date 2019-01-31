f5ca8c3c465bdda3e9643bd3a58ee9f57ef572db
cervante <luca.panzavolta@gmail.com>
2019-01-31 22:11:34 +0100

    Controller getOrdersFromUser done.

--- Controllers/UserControllers/getOrdersFromUser.js
+++ Controllers/UserControllers/getOrdersFromUser.js
@@ -1,13 +1,23 @@
 import sequelize from '../../db';
 
 const getOrdersFromUser = async (req, res) => {
-  const { userId } = req.body;
-  if (userId) {
-    const previousOrders = await sequelize.query(`SELECT * FROM orders WHERE user_id = ${userId}`);
+  try {
+    const { userId } = req.body;
+    if (userId) {
+      const [previousOrders] = await sequelize.query(`SELECT * FROM orders WHERE user_id = ${userId}`);
 
+      res
+        .status(200)
+        .send(previousOrders);
+    } else {
+      throw new Error('User is not logged in.');
+    }
+  } catch (err) {
+    //  eslint-disable-next-line
+    console.error('Error in getOrdersFromUser controller =>', err);
     res
-      .status(200)
-      .send(previousOrders);
+      .status(400)
+      .send('Unable to retrieve orders.');
   }
 };
 
