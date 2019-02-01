3e91f73852bb718870fe3c551f90f96ef2b1daf6
Christian Francia <CTFrancia@gmail.com>
2019-02-01 13:56:38 +0100

    chore: admin can correctly insert into products and product_property table(s)

--- Controllers/AdminControllers/admin.postNewProduct.js
+++ Controllers/AdminControllers/admin.postNewProduct.js
@@ -3,8 +3,8 @@ import sequelize from '../../db';
 import Product from '../../Models/ProductModel';
 import pP from '../../Models/Product_PropertiesModel';
 import QUERIES from '../rawqueries';
-// const Sequelize = require('sequelize');
 
+let wasWritten = false;
 const postNewProduct = async (req, res) => {
   /* eslint-disble */
 
@@ -23,11 +23,10 @@ const postNewProduct = async (req, res) => {
   if (req.method !== 'POST') {
     throw new Error('Improper Method');
   }
-
   try {
     const { vatRate } = CONSTANTS.vatRate;
-    const wasWrittenTest1 = await sequelize.query(`${QUERIES.insertIntoProducts}`,
-      {
+    await sequelize
+      .query(`${QUERIES.insertIntoProducts}`, {
         model: Product,
         replacements: {
           name,
@@ -41,44 +40,53 @@ const postNewProduct = async (req, res) => {
         },
         type: sequelize.QueryTypes.INSERT,
       })
-      .then(() => true);
-    // consoling wasWrittenTest1 comes back as true
-
-    if (wasWrittenTest1 === true) {
-      await Promise.all(productProperties.map(
-        async (obj) => {
-          // console.log(obj);
-          const answer = await sequelize.query(`${QUERIES.insertIntoProducts}`,
-            {
-              model: pP,
-              replacements: {
-                category_id: obj.category_id,
-                property_name: obj.property_name,
-                units: obj.units,
-                property_value: obj.property_value,
-              },
-              type: sequelize.QueryTypes.INSERT,
-            })
-            .then(() => true);
-          return answer;
-        },
-      ));
+      .then(() => {
+        wasWritten = true;
+      })
+      .catch((e) => {
+      /* eslint-disable-next-line */
+        console.log(e);
+        wasWritten = false;
+      });
+    try {
+      await Promise.all(
+        productProperties.map(obj => sequelize
+          .query(`${QUERIES.insertIntoProductProperties}`, {
+            model: pP,
+            replacements: {
+              category_id: obj.category_id,
+              property_name: obj.property_name,
+              units: obj.units,
+              property_value: obj.property_value,
+            },
+            type: sequelize.QueryTypes.INSERT,
+          })
+          .then(() => {
+            wasWritten = true;
+          })
+          .catch(() => {
+            wasWritten = false;
+          })),
+      );
+    } catch (e) {
+      /* eslint-disable-next-line */
+      console.log(e);
     }
-    // console.log('asdfhasdkjhfasjhdf', wasWrittenTest2);
-
-    if (wasWrittenTest1) {
-      res.status(201)
+    if (wasWritten) {
+      res
+        .status(201)
         .send('Success')
         .end();
     } else {
       res
         .status(500)
-        .send("Don't know bro but something happened")
+        .send('Was unable to save correctly, please try again later')
         .end();
     }
   } catch (e) {
-  // the response is an array with [id#, column#]
-    // console.log(e);
+    // the response is an array with [id#, column#]
+    /* eslint-disable-next-line */
+    console.log(e);
   }
 };
 
