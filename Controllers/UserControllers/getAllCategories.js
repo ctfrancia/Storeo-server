26e1d7e90c9e73d5f6f8fd9345cc2abfc1ea4adf
cervante <luca.panzavolta@gmail.com>
2019-02-01 10:05:05 +0100

    Refactored some controllers to deal with a changed way of exporting the Models.

--- Controllers/UserControllers/getAllCategories.js
+++ Controllers/UserControllers/getAllCategories.js
@@ -1,13 +1,21 @@
+import Sequelize from 'sequelize';
 import sequelize from '../../db';
+import { db } from '../../Models';
+
+const { Category } = db;
 
 const getAllCategories = async (req, res) => {
   try {
-    const [categories] = await sequelize.query(
+    const categories = await sequelize.query(
       `SELECT categories.id, categories.name, 
        GROUP_CONCAT(category_properties.property_name) AS property_names, 
        GROUP_CONCAT(category_properties.units) AS property_units
        FROM categories, category_properties
        WHERE categories.id = category_properties.category_id GROUP BY categories.id;`,
+      {
+        model: Category,
+        type: Sequelize.QueryTypes.SELECT,
+      },
     );
     res
       .status(200)
