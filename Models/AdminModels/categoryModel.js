c905026324a16169ab19431d5162ed52740a913a
Christian Francia <CTFrancia@gmail.com>
2019-02-03 11:57:16 +0100

    chore: add admin category update and update postman collection with route

--- Models/AdminModels/categoryModel.js
+++ Models/AdminModels/categoryModel.js
@@ -2,8 +2,6 @@ import sequelize from '../../db';
 import QUERIES from '../rawqueries';
 
 function updateCategory(name, id) {
-  console.log(name, id);
-
   return sequelize.query(`${QUERIES.updateCategory}`, {
     replacements: {
       name,
