606de9adb83150541f2596096dc7b57d474f9dcc
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-02-03 15:54:49 +0100

    RLint `postNewOrder` controller

--- Controllers/UserControllers/postNewOrder.js
+++ Controllers/UserControllers/postNewOrder.js
@@ -1,41 +1,17 @@
-import Sequelize from 'sequelize';
-
-import Order from '../../Schemas/OrderModel';
-import sequelize from '../../db';
+import insertNewOrder from '../../Models/AdminModels/insertNewOrder';
 
 const postNewOrder = async (req, res) => {
-// Deconstruct the `req` object, & rename values to
-// camelCase notation [ (from) req_prop_name: (to) reqPropName]
   const {
-    order_status: orderStatus,
-    special_instructions: specInstructions,
-    user_id: userId,
+    user,
+    special_instructions: instructions,
+    ordered_items: orderedItems,
   } = req.body;
 
-  const orderNum = 'PENDING TO MAKE DYNAMIC UUID';
-
   try {
-    const result = await sequelize.query(
-      `INSERT INTO orders
-      (order_num, order_status, special_instructions, user_id)
-      VALUES ( :orderNum, :orderStatus, :specInstructions, :userId)
-      `,
-      {
-        model: Order,
-        replacements: {
-          orderNum,
-          orderStatus,
-          specInstructions,
-          userId,
-        },
-        type: Sequelize.QueryTypes.INSERT,
-      },
-    );
-    // format before sending the response
-    const formattedResult = result[0][0];
+    const result = await insertNewOrder(user, instructions, orderedItems);
     res
       .status(201)
-      .send(formattedResult);
+      .send(result);
   } catch (err) {
     // eslint-disable-next-line
     console.error('Error in postNewOrder controller =>', err);
---
+++ Models/AdminModels/insertNewOrder.js
@@ -0,0 +1,55 @@
+import Sequelize from 'sequelize';
+import uuidv4 from 'uuid/v4';
+
+import Order from '../../Schemas/OrderModel';
+import sequelize from '../../db';
+
+import insertNewOrderedItem from './insertNewOrderedItem';
+
+const insertNewOrder = async (userObj, specInstructions, orderedItems) => {
+  const {
+    id: userId, first_name: fName, last_name: lName, email, phone,
+  } = userObj;
+  const orderNum = uuidv4();
+  const orderStatus = 0;
+  // PENDING TO DEFFINE STATUS CODES AND THE SETTER FUNCTION
+
+  const order = await sequelize.query(
+    `INSERT INTO orders
+    (order_num, order_status, special_instructions, user_id)
+    VALUES ( :orderNum, :orderStatus, :specInstructions, :userId)
+    `,
+    {
+      model: Order,
+      replacements: {
+        orderNum,
+        specInstructions,
+        userId,
+        orderStatus,
+      },
+      type: Sequelize.QueryTypes.INSERT,
+    },
+  );
+  const orderId = order[0];
+
+  // eslint-disable-next-line
+  const orderedItemsInsertionResult = await Promise.all(
+    orderedItems.map(orderedItem => insertNewOrderedItem(orderedItem, orderId)),
+  );
+
+  // Format before sending the response
+  const orderDetails = {
+    first_name: fName,
+    last_name: lName,
+    special_instructions: specInstructions,
+    email,
+    phone,
+    order_num: orderNum,
+    // ordered_items: orderedItemsInsertionResult,
+  };
+
+
+  return orderDetails;
+};
+
+export default insertNewOrder;
