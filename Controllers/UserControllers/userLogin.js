7cabe080f1f7d5a9c0401f5938c542c9359843a1
cervante <luca.panzavolta@gmail.com>
2019-02-04 15:28:07 +0100

    Modified login controller. Now working as expected.

--- Controllers/UserControllers/userLogin.js
+++ Controllers/UserControllers/userLogin.js
@@ -2,7 +2,6 @@ import bcrypt from 'bcrypt';
 import atob from 'atob';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import log from '../../Helpers/log';
 import sequelize from '../../db';
 import User from '../../Schemas/UserModel';
 
@@ -10,36 +9,44 @@ dotenv.config();
 
 const userLogin = async (req, res) => {
   const [authType, encodedString] = req.headers.authorization.split(' ');
+
   if (authType === 'Basic') {
     const [email, password] = atob(encodedString).split(':');
 
-    const [user] = await sequelize.query('SELECT id, first_name, last_name, password, role FROM users WHERE email = :email',
-      { replacements: { email }, type: sequelize.QueryTypes.SELECT });
+    const [user] = await sequelize.query(
+      'SELECT id, first_name, last_name, password, auth_token, role FROM users WHERE email = :email;',
+      {
+        replacements: {
+          email,
+        },
+        type: sequelize.QueryTypes.SELECT,
+      },
+    );
 
-    user.role = (user.role === 0) ? 'user' : 'admin';
-
-    log('user is ', user);
     //  The following block of code runs if the user with that email is found
     if (user) {
       const isPasswordValid = await bcrypt.compare(password, user.password);
+      user.role = (user.role === 0) ? 'user' : 'admin';
 
-      log('isPasswordValid', isPasswordValid);
       if (isPasswordValid) {
-        const jwtSecret = process.env.JWT_SECRET;
-        // Token generation
-        const token = jwt.sign({ email }, jwtSecret);
-        // Insert token for user in the DB
-        await sequelize.query(
-          'UPDATE users SET auth_token = ? WHERE email = ?',
-          {
-            model: User,
-            replacements: [token, email],
-            type: sequelize.QueryTypes.INSERT,
-          },
-        );
+        // If user does not have a token create one
+        if (!user.auth_token) {
+          const jwtSecret = process.env.JWT_SECRET;
+          // Token generation
+          const token = jwt.sign({ email }, jwtSecret);
+          // Insert token for user in the DB
+          await sequelize.query(
+            'UPDATE users SET auth_token = ? WHERE email = ?',
+            {
+              model: User,
+              replacements: [token, email],
+              type: sequelize.QueryTypes.INSERT,
+            },
+          );
+          user.auth_token = token;
+        }
         // Deleting password property on user object before sending to client
         delete user.password;
-        user.token = token;
 
         res
           .status(200)
