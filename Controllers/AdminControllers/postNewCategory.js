35c3f1ca6e1d03c2aba7bcea63950d0eb4eecd6d
Christian Francia <CTFrancia@gmail.com>
2019-02-05 18:47:51 +0100

    fix: when submitting duplicate categories proper message is returned

--- Controllers/AdminControllers/postNewCategory.js
+++ Controllers/AdminControllers/postNewCategory.js
@@ -3,10 +3,16 @@ import insertNewCategory from '../../Models/AdminModels/insertNewCategory';
 
 const postNewCategory = async (req, res) => {
   try {
-    await insertNewCategory(req.body);
-    res
-      .status(200)
-      .send('Success.');
+    const answer = await insertNewCategory(req.body);
+    if (answer === true) {
+      res
+        .status(409)
+        .end('Category already exists');
+    } else {
+      res
+        .status(200)
+        .send('Success.');
+    }
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error('Error in postNewCategory controller', err);
