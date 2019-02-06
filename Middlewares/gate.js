2965e23973ce918071eab8dad26fa59ca12c3f6b
cervante <luca.panzavolta@gmail.com>
2019-02-06 10:50:54 +0100

    Console.log statement in gate middleware for debugging.

--- Middlewares/gate.js
+++ Middlewares/gate.js
@@ -3,6 +3,8 @@ const gateMiddleware = (req, res, next) => {
   const { user } = req.body;
   // Determine if the request comes from admin or user
   const reqFromAdmin = req.originalUrl.includes('/admin');
+  // eslint-disable-next-line
+  console.log('Request from', req.body.user.role, 'and requested path is ', req.originalUrl);
   if (reqFromAdmin && user.role === 1) {
     next();
   } else if (reqFromAdmin && user.role !== 1) {
