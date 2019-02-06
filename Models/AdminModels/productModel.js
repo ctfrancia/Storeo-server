2a1ec34ad6adfef73a29d2f9504d6e36442764be
Christian Francia <CTFrancia@gmail.com>
2019-02-06 10:40:23 +0100

    fix: product create

--- Models/AdminModels/productModel.js
+++ Models/AdminModels/productModel.js
@@ -9,11 +9,9 @@ function deleteProduct(productId) {
   });
 }
 
-function addProduct(toBeInserted) {
+async function addProduct(toBeInserted) {
   // first query the db to see if the product already exists;
-  const exists = sequelize.query(`SELECT name FROM products WHERE name = "${toBeInserted.name}"`);
-
-  console.log(exists);
+  const exists = await sequelize.query(`SELECT name FROM products WHERE name = "${toBeInserted.name}"`);
 
   // if there is a length then we know that the product with the name already exists
   if (exists[0].length === 1) return true;
